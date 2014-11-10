16f586d27c49049dd623668a410979e994e8df2b
David Westreicher <david.westreicher@gmail.com>
2014-11-11 00:35:11 +0100

    fix the build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,25 @@ export LD_LIBRARY_PATH="native"
 export GALLIUM_HUD=fps,cpu+cpu0+cpu1+cpu2+cpu3:100
 export vblank_mode=0
 
+if [ ! -d "$LD_LIBRARY_PATH" ]; then
+	echo "You have no native folder! It should contain the dll's/so's for libawesomium and Jinput!"
+	exit 0
+fi
+
+if [ ! -d "$1" ]; then
+	echo "The specified gamefolder $1 doesn't exist!"
+	exit 0
+fi
+
 if [ ! -d "build" ]; then
 	mkdir build
 fi
-javac -cp .:libs/jna/*:libs/jogl/*:libs/jama/*:libs/joal/*:libs/gson/*:libs/ode4j/*:libs/vecmath/*:libs/rift/*:libs/jinput/*:libs/stackalloc/* -sourcepath src -d build src/**/*.java src/**/**/*.java
 
-java -Djava.library.path=$LD_LIBRARY_PATH -Djna.library.path=$LIBRARY_PATH -cp build/:libs/jna/*:libs/jogl/*:libs/jama/*:libs/joal/*:libs/gson/*:libs/ode4j/*:libs/vecmath/*:libs/rift/*:libs/jinput/*:libs/stackalloc/* test.OpenGLTest games/engineoverhaul/ engine/engine.cfg 
+for i in libs/**/*.jar; do
+    CLASSPATH=$CLASSPATH:$i
+done
+CLASSPATH=`echo $CLASSPATH | cut -c2-`
+
+javac -cp $CLASSPATH -sourcepath src -d build src/**/*.java src/**/**/*.java
+
+java -Djava.library.path=$LD_LIBRARY_PATH -Djna.library.path=$LIBRARY_PATH -cp build/:$CLASSPATH test.OpenGLTest $1 engine/engine.cfg 
